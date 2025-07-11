ee4ff733c045f7e65f8e3763a9650a3842682347
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2025-07-11 19:16:00 -0300

    game map

--- src/lib/helperFns.ts
+++ src/lib/helperFns.ts
@@ -30,3 +30,7 @@ export function randRange(min: number, max: number) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export async function sleep(time: number) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
